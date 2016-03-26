a508f93dcac45f03b87a0aebbbb1a66775cae64c
Taijianda Tan <johntan@Taijiandas-MacBook-Pro.local>
2016-03-26 11:46:40 -0700

    stage 1

--- stages/1/Main.elm
+++ stages/1/Main.elm
@@ -4,6 +4,20 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+type alias SearchResult =
+  { id : Int
+  , name : String
+  , stars : Int
+  }
+
+
+type alias Model =
+  { query : String
+  , result : List SearchResult
+  }
+
+
+model : Model
 model =
   { result =
       { id = 1
@@ -13,19 +27,26 @@ model =
   }
 
 
+view : Model -> Html
 view model =
   div
     [ class "content" ]
     [ header
         []
         [ -- TODO add the equivalent of <h1>ElmHub</h1> right before the tagline
-          span [ class "tagline" ] [ text "“Like GitHub, but for Elm things.”" ]
+          h1 [] [ text "ElmHub" ]
+        , span
+            [ class "tagline" ]
+            [ (text "“Like GitHub, but for Elm things.”") ]
         ]
     , ul
         [ class "results" ]
         [ li
             []
-            [ span [ class "star-count" ] [{- TODO display the number of stars -}]
+            [ span
+                [ class "star-count" ]
+                [ text (toString model.result.stars) ]
+            , a [ href ("https://github.com/" ++ model.result.name) ] [ text ("wowlink") ]
               -- TODO use the model to put a link here that points to
               -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
             ]
@@ -33,5 +54,6 @@ view model =
     ]
 
 
+main : Html
 main =
   view model
